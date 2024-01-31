c7e42b424fdc5de9112462e7bda78f9a95357d0f
nmukassa1 <78985121+nmukassa1@users.noreply.github.com>
2024-01-31 21:03:16 +0000

    render film scheduel via useState

--- src/Components/FilmScheduel.jsx
+++ src/Components/FilmScheduel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useData from '../Hooks/useData'
 import { useParams } from "react-router-dom";
 
@@ -7,63 +7,72 @@ function FilmScheduel() {
     const {filmDb, locationShowings, newDaysOfWeek} = useData()
     const {id, location} = useParams();
     const [upcomingShowingToday, setUpcomingShowingToday] = useState(`TODAY`)
+    const [filmScheduel, setFilmScheduel] = useState([])
 
     
 
     // OBJECTIVE: Find the correct movie the user has clicked 
-    let cinema
-    let globalScheduel // This acts as the scheduel for the whole week of every film
-    let currentFilmScheduel = []
-    if (locationShowings) {
-        //  go through array and find the object that matches the url location slug
-        cinema = locationShowings.find((item) => item.location === location )
+    let cinemaLocation
+    let locationScheduel // This acts as the scheduel for the whole week of every film
 
-        //turn scheduel object into an array so I can  itterate over it
-        globalScheduel = Object.values(cinema.schedual)
+    let currentFilmScheduel = []
+
+    useEffect(() => {
+        setFilmScheduel(currentFilmScheduel)
+    }, [])
+
+    if (locationShowings) {
+        //  location of cinema
+        cinemaLocation = locationShowings.find((item) => item.location === location )
+
+        // scheduel for week
+        locationScheduel = Object.values(cinemaLocation.schedual)
 
         // Isolate the schedule for the wwek for the current film I need right now
-        // Itterate over the globalScheduel array
-        for(let i = 0; i < globalScheduel.length; i++){
+        // Itterate over the locationScheduel array
+        for(let i = 0; i < locationScheduel.length; i++){
             // Isolate each day
-            let day = globalScheduel[i].date;
+            let day = locationScheduel[i].date;
 
             // Itterate over the movie array so I can find which item matches the search query / movie I'm looking for. 
-            globalScheduel[i].movies.forEach((movie) => {
+            locationScheduel[i].movies.forEach((movie) => {
                 if(movie.id === id) currentFilmScheduel.push({day: day, time_screen: movie.time_screen})
+                // setFilmScheduel(currentFilmScheduel)
             })
         }
     }
-    console.log(cinema)
 
     return ( 
         <div className="schedule">
-            {currentFilmScheduel.map((item, index) => (
-                <div key={index}>
-                    <h2>
-                        {index === 0 && `UPCOMING SHOWINGS FOR ${upcomingShowingToday}`}
-                        {index !== 0 && newDaysOfWeek[index]}
-                        </h2> 
-                    <ul className="card-schedule-container">
-                        {item.time_screen.map((item, index) => (
-                            <li key={index}>
-                                <button className="card-schedule">
-                                    <div>
-                                        <div className="time">
-                                            <span>{item.time}</span>
+            {filmScheduel.length > 0 && (
+                filmScheduel.map((item, index) => (
+                    <div key={index}>
+                        <h2>
+                            {index === 0 && `UPCOMING SHOWINGS FOR ${upcomingShowingToday}`}
+                            {index !== 0 && newDaysOfWeek[index]}
+                            </h2> 
+                        <ul className="card-schedule-container">
+                            {item.time_screen.map((item, index) => (
+                                <li key={index}>
+                                    <button className="card-schedule">
+                                        <div>
+                                            <div className="time">
+                                                <span>{item.time}</span>
+                                            </div>
+                                            <div className="screen">
+                                                <span>Screen {item.screen}</span>
+                                            </div>
                                         </div>
-                                        <div className="screen">
-                                            <span>Screen {item.screen}</span>
+                                        <div className="price">
+                                            <span>FROM £4.99</span>
                                         </div>
-                                    </div>
-                                    <div className="price">
-                                        <span>FROM £4.99</span>
-                                    </div>
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ))}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))
+            )}
         </div>
     );
 }
